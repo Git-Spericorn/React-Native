e462eb1216df0159f8b761f3cc0d76a0e3a7d0e2
Rahul Dev <albina@apportunity.io>
2019-12-13 10:46:35 +0530

    first commit

--- sample.js
+++ sample.js
@@ -1,26 +1,26 @@
-## export multiple functions
+// export multiple functions
+export default function createMixpanel(store) {
+  const mixpanel = Object.create(Mixpanel);
 
-`export default function createMixpanel(store) {
-const mixpanel = Object.create(Mixpanel);
+  mixpanel.store = store;
+  mixpanel.people = {
+    set: People.set.bind(mixpanel)
+  };
 
-mixpanel.store = store;
-mixpanel.people = {
-set: People.set.bind(mixpanel)
-};
+  mixpanel.\_distinct_id = null;
+  mixpanel.\_superProperties = {};
+  mixpanel.\_profileSetQueue = [];
+  mixpanel.\_eventQueue = [];
 
-mixpanel.\_distinct_id = null;
-mixpanel.\_superProperties = {};
-mixpanel.\_profileSetQueue = [];
-mixpanel.\_eventQueue = [];
+  return mixpanel;
+}
 
-return mixpanel;
-}`
+// google analytics handlers
 
-## analytics handlers
-
-`export const ROUTE_MAP = {};
+export const ROUTE_MAP = {};
 ROUTE_MAP["EditEventModal"] = route => {
-  if (Object.keys(route.params || {}).length === 0) return { eventName: "Create Event Tapped" };
+  if (Object.keys(route.params || {}).length === 0)
+    return { eventName: "Create Event Tapped" };
 };
 ROUTE_MAP["DatingHome"] = route => ({ eventName: "Dating Tab Visited" });
 ROUTE_MAP["RecoveryHome"] = route => ({ eventName: "Recovery Tab Visited" });
@@ -28,7 +28,6 @@ ROUTE_MAP["RecoveryHome"] = route => ({ eventName: "Recovery Tab Visited" });
 ROUTE_MAP["INBOX_HOME"] = route => {
   return { eventName: "Inbox View" };
 };
-
 
 OPERATION_MAP["CreateEvent"] = (operation, result) => {
   const event = result.data.createEvent;
@@ -41,8 +40,8 @@ OPERATION_MAP["CreateEvent"] = (operation, result) => {
       startsAt: event.startsAt,
       eventCategory: event.category,
       /** @todo This information is not currently tracked */
-      eventsCreated: null,
-    },
+      eventsCreated: null
+    }
   };
 };
 
@@ -52,14 +51,16 @@ OPERATION_MAP["AttendEvent"] = (operation, result) => {
 };
 OPERATION_MAP["LoginOrRegisterWithPhoneAndCode"] = (operation, result) => {
   const { firstRegistration } = result.data.loginOrRegisterWithPhoneAndCode;
-  return firstRegistration ? { eventName: "Register" } : { eventName: "Sign In" };
+  return firstRegistration
+    ? { eventName: "Register" }
+    : { eventName: "Sign In" };
 };
 OPERATION_MAP["LoginOrRegisterWithFacebook"] = (operation, result) => {
   const { firstRegistration } = result.data.loginOrRegisterWithFacebook;
-  return firstRegistration ? { eventName: "Register" } : { eventName: "Sign In" };
+  return firstRegistration
+    ? { eventName: "Register" }
+    : { eventName: "Sign In" };
 };
 OPERATION_MAP["JoinGroup"] = (operation, result) => {
   return { eventName: "Group Joined" };
 };
-
-`
